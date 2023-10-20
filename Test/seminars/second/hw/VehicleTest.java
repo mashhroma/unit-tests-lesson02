f18e94a763060f5884e2d3fed9d59db805a3914b
Mashhroma <mashh@mail.ru>
2023-10-20 22:21:53 +0300

    hw2 comment to teacher

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
-
+    /**Я разобралась!*/
     Car car;
     Motorcycle motorcycle;
 
